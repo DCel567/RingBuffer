2c05d702cee1db7428ca7b4925c42edc9d3723ef
George <dcel567@gmail.com>
2022-09-22 20:54:40 +0300

    Pointers cycling added

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -2,7 +2,8 @@
 
 class RingBuffer {
 private:
-	int16_t bSize = 0;
+	int16_t allocSize = 0;
+	int16_t realSize = 0;
 	char* buffer;
 	char* firstEl;
 	char* lastEl;
@@ -10,7 +11,7 @@ private:
 public:
 	RingBuffer(int32_t size) {
 		buffer = new char[size];
-		bSize = size;
+		allocSize = size;
 
 		firstEl = buffer;
 		lastEl = buffer;
@@ -21,8 +22,18 @@ public:
 	}
 
 	void push(const char& ch) {
+		++firstEl;
+		if (firstEl == buffer + allocSize) {
+			firstEl = buffer;
+		}
+		if (firstEl == lastEl){
+			++lastEl;
+			if (lastEl == buffer + allocSize)
+				lastEl = buffer;
+		}
 		*firstEl = ch;
-		firstEl = firstEl + 1;
+		if (realSize < allocSize)
+			++realSize;
 	}
 
 	/*char pop() {
@@ -30,8 +41,13 @@ public:
 	}*/
 
 	void print_buffer() const {
-		for (int i = 0; i < bSize; ++i) {
-			std::cout << i << ": " << buffer[i] << '\n';
+		for (int i = 0; i < allocSize; ++i) {
+			std::cout << i << ": " << buffer[i];
+			if (buffer + i == firstEl)
+				std::cout << "  --< first";
+			if (buffer + i == lastEl)
+				std::cout << "  --< last";
+			std::cout << '\n';
 		}
 	}
 
@@ -39,13 +55,20 @@ public:
 
 
 int main() {
-	RingBuffer rb = RingBuffer(10);
+	RingBuffer rb = RingBuffer(5);
 	rb.print_buffer();
 	std::cout << '\n';
 
 	rb.push('k');
 	rb.push('g');
 	rb.push('e');
+	rb.push('a');
+	rb.push('b');
+	rb.push('c');
+	rb.push('d');
+	rb.push('d');
+	rb.push('d');
+	rb.push('d');
 
 	rb.print_buffer();
 
